7205ad21aefb73feca4b678203b1a37c6eb32928
Osama Najjar <osama.najjar.m@gmail.com>
2024-02-17 09:45:10 +0100

    setup config module and services

--- service/src/app.service.ts
+++ service/src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly configService: ConfigService) {}
+
+  getHealthCheck(): string {
+    return this.configService.get<string>('HEALTH_CHECK_MESSAGE');
   }
 }
